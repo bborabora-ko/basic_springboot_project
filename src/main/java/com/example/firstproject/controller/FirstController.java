d328108cf1b7cde3e5b96dc7c04262011f06679f
bborabora-ko <bborabora.ko@gmail.com>
2022-08-15 22:38:23 +0900

    maked view template & controller after using model

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,13 +1,15 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // Controller선언
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "미미");
         return "greetings"; // templates/greetings.mustache -> 브라우저로 전송!
     }
 }
