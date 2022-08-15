8e1397aaadf81fff8367e7e185809e8134492aed
bborabora-ko <bborabora.ko@gmail.com>
2022-08-16 00:06:53 +0900

    added view template(/bye)

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -9,7 +9,18 @@ public class FirstController {
 
     @GetMapping("/hi")
     public String niceToMeetYou(Model model){
-        model.addAttribute("username", "미미");
-        return "greetings"; // templates/greetings.mustache -> 브라우저로 전송!
+        model.addAttribute("username", "미미"); //username이라는 변수 등록(값은 "미미")
+        return "greetings"; //뷰페이지의 이름 기재
+                            // templates/greetings.mustache (뷰페이지) -> 브라우저로 전송!
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
+
+
 }
+
+
