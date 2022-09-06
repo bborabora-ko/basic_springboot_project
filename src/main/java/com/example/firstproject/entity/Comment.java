868c3e9f578d205c98b1565205d6b2867c540e3b
bborabora-ko <bborabora.ko@gmail.com>
2022-09-07 01:43:04 +0900

    maked commentService & commentApiController

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import lombok.*;
 
 import javax.persistence.*;
@@ -24,4 +25,36 @@ public class Comment {
     @Column
     private String body;
 
+    public static Comment createComment(CommentDto dto, Article article) {
+        // 예외상황 시 예외발생
+        if (dto.getId() != null) { //id를 입력함(create시 body에 id 입력하면 안됨)
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글의 id가 없어야 합니다.");
+        }
+        if (dto.getArticleId() != article.getId()) {
+            throw new IllegalArgumentException("댓글 생성 실패! 게시글의 id가 잘못되었습니다.");
+        }
+        // 엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+    }
+
+    public void patch(CommentDto dto) {
+        // 예외 발생
+        if(this.id != dto.getId())
+            throw  new IllegalArgumentException("댓글 수정 실패! 잘못된 id가 입력되었습니다.");
+
+        // 객체를 갱신
+        if(dto.getNickname() != null){
+            this.nickname = dto.getNickname();
+        }
+
+        if(dto.getBody() != null){
+            this.body = dto.getBody();
+        }
+    }
 }
+
